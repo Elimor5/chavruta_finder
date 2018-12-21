5675eff51e6b7b10090ccfadbcfeb3ed7934c043
Eli Mor <emor0219@gmail.com>
2018-12-21 08:53:36 -0500

    gitigignore dist files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); 
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "app", "assets", "javascripts", "js"),
+    outputDir: path.resolve(__dirname, "app", "assets", "javascripts", "dist"),
     configureWebpack: {
         entry: path.resolve(__dirname, 'frontend', 'src', 'main.js')
     }
