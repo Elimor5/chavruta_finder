763d4920efc9ac2417c82187c889e3c7b45ac96d
Eli Mor <emor0219@gmail.com>
2018-12-24 15:12:30 -0500

    cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Chavruta Finder</div>
+      </v-ons-toolbar>
+
+      <p style="text-align: center">
+        <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
+      </p>
+    </v-ons-page>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
