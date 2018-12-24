6654f207dff685f9c4750decefbf9b027ec7c94d
Eli Mor <emor0219@gmail.com>
2018-12-24 15:51:34 -0500

    toolbar button added and app file reformatted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
-    <v-ons-page>
-      <v-ons-toolbar>
-        <div class="center">Chavruta Finder</div>
-      </v-ons-toolbar>
+    <div id="app">
+        <v-ons-page>
+            <v-ons-toolbar>
+                <div class="left">
+                    <v-ons-toolbar-button icon="ion-navicon, material: md-menu"></v-ons-toolbar-button>
+                </div>
 
-      <p style="text-align: center">
-        <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
-      </p>
-    </v-ons-page>
-  </div>
+                <div class="center">Chavruta Finder</div>
+            </v-ons-toolbar>
+
+            <p style="text-align: center">
+                <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
+            </p>
+        </v-ons-page>
+    </div>
 </template>
 
 <style lang="scss">
