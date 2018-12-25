7ec55477fdb715a2f26671035e62f98aeba017fa
Eli Mor <emor0219@gmail.com>
2018-12-24 19:50:55 -0500

    splitter working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,32 @@
 <template>
     <div id="app">
-        <v-ons-page>
+        <v-ons-splitter>
+            <v-ons-splitter-side
+                swipeable
+                width="200px"
+                collapse
+                side="left"
+                :open.sync="isSideOpen"
+            >
+                <v-ons-page>
+                    <v-ons-list>
+                        <v-ons-list-item
+                            tappable
+                        >
+                            <div class="center">test</div>
+                        </v-ons-list-item>
+                    </v-ons-list>
+                </v-ons-page>
+            </v-ons-splitter-side>
+
+            <v-ons-splitter-content>
+                <v-ons-page>
             <v-ons-toolbar>
                 <div class="left">
-                    <v-ons-toolbar-button icon="ion-navicon, material: md-menu"></v-ons-toolbar-button>
+                    <v-ons-toolbar-button
+                        icon="ion-navicon, material: md-menu"
+                        @click="ToggleSplitter"
+                    ></v-ons-toolbar-button>
                 </div>
 
                 <div class="center">Chavruta Finder</div>
@@ -13,8 +36,29 @@
                 <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
             </p>
         </v-ons-page>
+            </v-ons-splitter-content>
+        </v-ons-splitter>
+
+        
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            isSideOpen: false
+        };
+    },
+    methods: {
+        ToggleSplitter() {
+            this.isSideOpen = !this.isSideOpen;
+        }
+    }
+};
+</script>
+
+
 <style lang="scss">
 </style>
+
