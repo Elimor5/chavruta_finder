1de13701550ec38184d3d01c56ea7589e4ca05f8
Eli Mor <emor0219@gmail.com>
2018-12-25 10:02:50 -0500

    beit samech daled

--- frontend/src/components/pages/home/_partials/home-top-banner/HomeTopBanner.vue
+++ frontend/src/components/pages/home/_partials/home-top-banner/HomeTopBanner.vue
@@ -2,23 +2,16 @@
     <div class="homeTopBannerComponent">
         <div class="topBannerImage">
             <div class="topBannerLayout">
+                <p class="beitSamechDaled">בס״ד</p>
 
                 <div class="headingContainer">
-                    <h1 class="header">
-                        Chavruta Finder
-                    </h1>
-                    
-                    <h2>
-                        "אין התורה נקנית אלא בחבורה"
-                    </h2>
+                    <h1 class="header">Chavruta Finder</h1>
 
-                    <h4 class="subHead">
-                        "Torah is only acquired in a group"
-                    </h4>
+                    <h2>"אין התורה נקנית אלא בחבורה"</h2>
 
-                    <h5>
-                        -Berakhot 63b
-                    </h5>
+                    <h4 class="subHead">"Torah is only acquired in a group"</h4>
+
+                    <h5>-Berakhot 63b</h5>
                 </div>
             </div>
         </div>
@@ -26,34 +19,37 @@
 </template>
 
 <style lang="scss" scoped>
-    .topBannerImage {
-        position: relative;
+.topBannerImage {
+    position: relative;
+    width: 100%;
+    height: 300px;
+    background-image: url("https://s3-us-east-2.amazonaws.com/illumevents1-pro/events/images/000/000/511/original/relig.jpg?1545701061");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    .topBannerLayout {
+        height: 100%;
         width: 100%;
-        height: 300px;
-        background-image: url("https://s3-us-east-2.amazonaws.com/illumevents1-pro/events/images/000/000/511/original/relig.jpg?1545701061");
-        background-size: cover;
-        background-repeat: no-repeat;
-     
-        .topBannerLayout {
-            height: 100%;
-            width: 100%;
-            background: rgba(0, 0, 0, 0.5);
-            position: absolute;
+        background: rgba(0, 0, 0, 0.5);
+        position: absolute;
 
-            display: flex;
-            justify-content: center;
+        color: white;
 
-            .headingContainer {
-                color: white;
-                text-align: center;
+        .beitSamechDaled {
+            text-align: right;
+            padding-right: 10px;
+        }
 
-                .header {
-                    padding: 50px 0 30px 0;
-                    font-size: 40px;
-                }
+        .headingContainer {
+            text-align: center;
+
+            .header {
+                padding-top: 30px;
+                font-size: 40px;
             }
         }
     }
+}
 </style>
 
 
