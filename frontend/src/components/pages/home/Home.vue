37ccd9b625ec263e976f32719778cb4420e7f112
Eli Mor <emor0219@gmail.com>
2018-12-24 21:12:36 -0500

    layout added

--- frontend/src/components/pages/home/Home.vue
+++ frontend/src/components/pages/home/Home.vue
@@ -1,16 +1,17 @@
+
 <template>
-  <div class="home">
-   
-  </div>
+    <div class="home">
+        <HomeTopBanner></HomeTopBanner>
+    </div>
 </template>
 
 <script>
-
+import HomeTopBanner from './_partials/home-top-banner/HomeTopBanner.vue';
 
 export default {
-  name: 'home',
-  components: {
-  
-  }
-}
+    name: "home",
+    components: {
+        HomeTopBanner
+    }
+};
 </script>
---
+++ frontend/src/components/pages/home/_partials/home-top-banner/HomeTopBanner.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="homeTopBannerComponent">
+        <div class="topBannerImage">
+            <div class="topBannerLayout">
+
+                <div class="headingContainer">
+                    <h1 class="header">
+                        Chavruta Finder
+                    </h1>
+                    
+                    <h2>
+                        "אין התורה נקנית אלא בחבורה"
+                    </h2>
+
+                    <h4 class="subHead">
+                        "Torah is only acquired in a group"
+                    </h4>
+
+                    <h5>
+                        -Berakhot 63b
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .topBannerImage {
+        position: relative;
+        width: 100%;
+        height: 300px;
+        background-image: url("https://s3-us-east-2.amazonaws.com/illumevents1-pro/events/images/000/000/511/original/relig.jpg?1545701061");
+        background-size: cover;
+        background-repeat: no-repeat;
+     
+        .topBannerLayout {
+            height: 100%;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            position: absolute;
+
+            display: flex;
+            justify-content: center;
+
+            .headingContainer {
+                color: white;
+                text-align: center;
+
+                .header {
+                    padding: 50px 0 30px 0;
+                    font-size: 40px;
+                }
+            }
+        }
+    }
+</style>
+
+
