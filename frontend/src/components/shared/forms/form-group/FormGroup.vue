8b0f55f2bb8e8ee89bb34be9c22ff8e5283897c7
Eli Mor <emor0219@gmail.com>
2018-12-26 18:08:30 -0500

    form group finalized

--- frontend/src/components/shared/forms/form-group/FormGroup.vue
+++ frontend/src/components/shared/forms/form-group/FormGroup.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="formGroupComponent">
-        <v-ons-list-item modifier="nodivider">
+        <v-ons-list-item class="formGroupContainer" :class="{'success': IsSuccessState, 'error': IsErrorState}" modifier="nodivider">
             <div class="left">
                 <slot name="icon"></slot>
             </div>
             <label class="center">
                 <slot name="input"></slot>
+
+                <template v-if="ErrorMessage">
+                    <p class="error">
+                        {{ErrorMessage}}
+                    </p>
+                </template>
             </label>
         </v-ons-list-item>
-        <template v-for="(errorMessage, idx) in ErrorMessages">
-            <p class="error" :key="idx">
-                {{errorMessage}}
-            </p>
-        </template>
     </div>
 </template>
 
@@ -20,20 +21,34 @@
 export default {
     props: ["validation"],
     computed: {
-        ErrorMessages() {
+        ErrorMessage() {
             const { validation } = this;
             const errors = [];
 
-            if (!validation.$error) return errors;
+            if (!validation.$error)
+               return null;
 
-            if (validation.hasOwnProperty("required") && !validation.required) {
-                errors.push("this field is required");
+            if (validation.hasOwnProperty('required') && !validation.required) {
+                errors.push('this field is required');
             } 
-            else if (validation.hasOwnProperty("email") && !validation.email) {
-                errors.push("please enter a valid email address");
+            else if (validation.hasOwnProperty('email') && !validation.email) {
+                errors.push('please enter a valid email address');
+            }
+            else if (validation.hasOwnProperty('minLength') && !validation.minLength) {
+                errors.push(`minimum length must be at least ${validation.$params.minLength.min} characters`);
             }
 
-            return errors;
+            return errors[0];
+        },
+        IsErrorState() {
+            const {$invalid, $error} = this.validation;
+            
+            return $invalid && $error;
+        },
+        IsSuccessState() {
+            const {$invalid, $error} = this.validation;
+
+            return !$invalid && !$error;
         }
     }
 };
@@ -43,9 +58,37 @@ export default {
 .formGroupComponent {
     padding-top: 15px;
 
-    .inputField {
-        width: 100%;
-        padding-right: 20px;
+    .formGroupContainer {
+        height: 75px; 
+
+        .inputField {
+            width: 100%;
+            padding-right: 20px;
+        }
+
+        &.success {
+            .text-input--material{
+                &:focus {
+                    background-image: linear-gradient(green, green),
+                    linear-gradient(to top, transparent 1px, #afafaf 1px);
+                }
+            } 
+        }
+
+        &.error {
+            .text-input--material{
+                &:focus {
+                    background-image: linear-gradient(red, red),
+                    linear-gradient(to top, transparent 1px, #afafaf 1px);
+                }
+            } 
+        }
+        
+        .error {
+            margin: 0 0 3px 0;
+            font-size: 12px;
+            color: red;
+        }
     }
 }
 </style>
