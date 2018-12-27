05e3fa72b0c10cf4e1d21bc9e12c9bbf0be03464
Eli Mor <emor0219@gmail.com>
2018-12-26 20:28:37 -0500

    store added for loader

--- frontend/src/scripts/store.js
+++ frontend/src/scripts/store.js
@@ -4,13 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    modules: {
+        loader: {
+            strict: true,
+            namespaced: true,
+            state: {
+                isLoading: false
+            },
+            mutations: {
+                toggle(state, loadState) {
+                    state.isLoading = loadState;
+                }
+            }
+        },
+    }
 })
