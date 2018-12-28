5fa926304bf53bfa357e2475ee994283e07f9352
Eli Mor <emor0219@gmail.com>
2018-12-27 21:45:43 -0500

    fixed cors issue

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
 import baseUrl from './baseUrl';
 
+Axios.defaults.withCredentials = true;
+
 export function http(options) {
     return Axios({
         method: options.method,
