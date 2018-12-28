5686f617d5ca7c5e3856337ba24187153ba4eb7a
Eli Mor <emor0219@gmail.com>
2018-12-28 11:14:40 -0500

    log in route guard

--- frontend/src/scripts/router.js
+++ frontend/src/scripts/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import store from "./store/store";
+
 import StatusCall from './startup/statusCall';
 
 import Home from "../components/pages/home/Home.vue";
@@ -20,7 +22,15 @@ const router = new Router({
       {
           path: "/auth/login",
           name: "Log In",
-          component: Login
+          component: Login,
+          beforeEnter(to, from, next) {
+              if (store.state.identity.isAuthenticated) {
+                  next({path: '/'});
+              }
+              else {
+                  next();
+              }
+          }
       }
   ]
 });
