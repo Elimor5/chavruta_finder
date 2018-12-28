e137a4a9fad0e06df583ca0bfc98760def356194
Eli Mor <emor0219@gmail.com>
2018-12-28 00:38:14 -0500

    splitter content extracted and avatar created

---
+++ frontend/src/components/shared/splitter-content/SplitterContent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="splitterContentComponent">
+        <v-ons-page>
+            <template v-if="IsLoggedIn">
+                <UserAvatar></UserAvatar>
+            </template>
+            <template v-else>
+                <v-ons-list>
+                    <router-link :to="{name:'Log In'}">
+                        <v-ons-list-item modifier="longdivider" tappable>
+                            <div class="center">Log In</div>
+                        </v-ons-list-item>
+                    </router-link>
+                    <v-ons-list-item modifier="longdivider" tappable>
+                        <div class="center">Sign Up</div>
+                    </v-ons-list-item>
+                </v-ons-list>
+            </template>
+        </v-ons-page>
+    </div>
+</template>
+
+<script>
+import UserAvatar from '../user-avatar/UserAvatar.vue';
+
+export default {
+    components: {
+        UserAvatar
+    },
+    computed: {
+        IsLoggedIn() {
+            return this.$store.getters['identity/isAuthenticated'];
+        }
+    }
+}
+</script>
