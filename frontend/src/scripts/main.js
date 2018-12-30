05a5bc0a580c5d687e1d001a4745e0e07de5212a
Eli Mor <emor0219@gmail.com>
2018-12-30 17:30:39 -0500

    vue file on bottom

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from '../App.vue';
-import FormGroup from '../components/shared/forms/form-group/FormGroup.vue';
 import router from './router';
 import store from './store/store';
 
@@ -10,38 +9,37 @@ import VueOnsen from 'vue-onsenui';
 
 import Vuelidate from 'vuelidate';
 
+import FormGroup from '../components/shared/forms/form-group/FormGroup.vue';
+
 Vue.config.productionTip = false
 
 Vue.component('cf-formgroup', FormGroup);
 
 document.addEventListener("DOMContentLoaded", function() {
-  const el = document.getElementById('app');
+    const el = document.getElementById('app');
 
-  Vue.use(VueOnsen);
-  Vue.use(Vuelidate);
+    Vue.use(VueOnsen);
+    Vue.use(Vuelidate);
 
-  new Vue({
-    router,
-    store,
-    render: h => h(App),
-    beforeCreate() {
-        Vue.prototype.$loader = { 
-            show: () => {
-                this.$store.commit('loader/toggle', true);
-            },
-            hide: () => {
-                this.$store.commit('loader/toggle', false);
-            }
-        };
-        
-        Vue.prototype.$toastr = {
-            toast: (message) => {
-                this.$store.dispatch('toastr/toast', message);
+    new Vue({
+        router,
+        store,
+        render: h => h(App),
+        beforeCreate() {
+            Vue.prototype.$loader = {
+                show: () => {
+                    this.$store.commit('loader/toggle', true);
+                },
+                hide: () => {
+                    this.$store.commit('loader/toggle', false);
+                }
+            };
+
+            Vue.prototype.$toastr = {
+                toast: (message) => {
+                    this.$store.dispatch('toastr/toast', message);
+                }
             }
         }
-    }
-  }).$mount(el)
+    }).$mount(el)
 });
-
-
-
