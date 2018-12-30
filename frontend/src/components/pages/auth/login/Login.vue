af46e39b76488ce157c38f856ebca2a4c6eefb37
Eli Mor <emor0219@gmail.com>
2018-12-30 01:18:41 -0500

    custom submit button

--- frontend/src/components/pages/auth/login/Login.vue
+++ frontend/src/components/pages/auth/login/Login.vue
@@ -3,61 +3,55 @@
         <div class="iconContainer">
             <v-ons-icon icon="md-account-box" class="list-item__icon"></v-ons-icon>
         </div>
-        <v-ons-list class="formInputContainer">
-            <cf-formgroup :validation="$v.Form.Username">
-                <template slot="icon">
-                    <v-ons-icon icon="fa-user" class="list-item__icon"></v-ons-icon>
-                </template>
-                <template slot="input">
-                    <v-ons-input
-                        class="inputField"
-                        float="true"
-                        placeholder="Username"
-                        v-model="Form.Username"
-                    ></v-ons-input>
-                </template>
-            </cf-formgroup>
-            <cf-formgroup :validation="$v.Form.Password">
-                <template slot="icon">
-                    <v-ons-icon icon="fa-key" class="list-item__icon"></v-ons-icon>
-                </template>
-                <template slot="input">
-                    <v-ons-input
-                        class="inputField"
-                        float="true"
-                        placeholder="Password"
-                        type="password"
-                        v-model="Form.Password"
-                    ></v-ons-input>
-                </template>
-            </cf-formgroup>
-        
-            <v-ons-button
-                type="submit"
-                modifier="large"
-                class="authSubmitButton"
-                @click="SubmitForm"
-            >Log in</v-ons-button>
-        </v-ons-list>
+
+        <form @submit.prevent="SubmitForm">
+            <v-ons-list class="formInputContainer">
+                <cf-formgroup :validation="$v.Form.Username">
+                    <template slot="icon">
+                        <v-ons-icon icon="fa-user" class="list-item__icon"></v-ons-icon>
+                    </template>
+                    <template slot="input">
+                        <v-ons-input
+                            class="inputField"
+                            float="true"
+                            placeholder="Username"
+                            v-model="Form.Username"
+                        ></v-ons-input>
+                    </template>
+                </cf-formgroup>
+                <cf-formgroup :validation="$v.Form.Password">
+                    <template slot="icon">
+                        <v-ons-icon icon="fa-key" class="list-item__icon"></v-ons-icon>
+                    </template>
+                    <template slot="input">
+                        <v-ons-input
+                            class="inputField"
+                            float="true"
+                            placeholder="Password"
+                            type="password"
+                            v-model="Form.Password"
+                        ></v-ons-input>
+                    </template>
+                </cf-formgroup>
+
+                <SubmitButton>Log In</SubmitButton>
+            </v-ons-list>
+        </form>
     </div>
 </template>
 
 <script>
 import LoginForm from "../../../../scripts/forms/auth/LoginForm.js";
+import SubmitButton from "../../../shared/buttons/submit-button/SubmitButton.vue"
 
 export default {
+    components: {
+        SubmitButton
+    },
     data() {
         return {
             Form: LoginForm.getDefaultData()
         };
-    },
-    computed: {
-        ReturnUrl() {
-            const fullPath = this.$route.query.returnUrl;
-            const baseUrl = window.location.origin;
-
-            return fullPath.split(baseUrl)[1];
-        }
     },
     validations: LoginForm.getValidations(),
     methods: {
@@ -71,13 +65,11 @@ export default {
             this.$loader.show();
 
             try {
-                const convertedFormData = LoginForm.convertFormData(this.Form)
-                
+                const convertedFormData = LoginForm.convertFormData(this.Form);
+
                 const response = await LoginForm.submit(convertedFormData);
-                
-                this.$router.push(this.ReturnUrl)
-            }
-            catch(e) {
+                this.$router.push(this.$route.query.returnUrl || '/');
+            } catch (e) {
                 this.$toastr.toast(e);
             }
 
@@ -102,10 +94,5 @@ export default {
     padding-bottom: 20px;
 
     text-align: center;
-
-    .authSubmitButton {
-        margin-top: 15px;
-        width: 90%;
-    }
 }
 </style>
---
+++ frontend/src/components/shared/buttons/submit-button/SubmitButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="submitButtonComponent" @click="HandleClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        HandleClick() {
+            this.$emit('click');
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .submitButtonComponent {
+        margin-top: 15px;
+        width: 90%;
+        height: 35px;
+        background: #0076ff;
+        color: white;
+        font-size: 17px;
+        font-weight: bold;
+        border-radius: 5%;
+    }
+</style>
+
