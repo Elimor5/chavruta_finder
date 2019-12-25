62711b7de41dc6c63187076fe4facf4939924fa2
Eli Mordechai <emor0219@gmail.com>
2019-12-25 13:49:00 -0500

    updated cors and baseurl

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'http://192.168.1.5:8080'
+    origins 'http://localhost:8080', 'http://192.168.1.5:8080', 'https://chavruta-finder.herokuapp.com/',
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head),
