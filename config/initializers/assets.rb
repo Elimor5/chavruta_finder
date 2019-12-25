bbf1dc9e338cc0bf3aa5cf342055364fc7c7f9f5
Eli Mordechai <emor0219@gmail.com>
2019-12-25 10:51:24 -0500

    attermpt to not precompile woff files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+Rails.application.config.assets.paths << Rails.root.join('node_modules')Rails.application.config.assets.precompile << Proc.new do |path|
+    if path =~ /\.(woff|woff2)\z/
+      false
+  end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
