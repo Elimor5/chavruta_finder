7419af1b8bc9094e7dac02a255be03e87bb1cb57
Eli Mordechai <emor0219@gmail.com>
2019-12-25 10:55:20 -0500

    added space

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')Rails.application.config.assets.precompile << Proc.new do |path|
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.precompile << Proc.new do |path|
     if path =~ /\.(woff|woff2)\z/
       false
   end
