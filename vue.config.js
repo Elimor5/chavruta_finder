c66dae6f0c77aad70f1badb67ea3c312e5d3417b
Eli Mordechai <emor0219@gmail.com>
2019-12-25 01:03:34 -0500

    attempt to fix bug with assets

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "app", "assets", "javascripts", "dist"),
+  assetsDir: '../../../assets/',
   configureWebpack: {
     entry: path.resolve(__dirname, "frontend", "src", "scripts", "main.js")
   },
