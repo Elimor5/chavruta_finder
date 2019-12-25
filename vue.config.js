c38fe814e39eea6a60ccff79811886859b34ecbc
Eli Mordechai <emor0219@gmail.com>
2019-12-25 01:32:41 -0500

    trying chain webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "app", "assets", "javascripts", "dist"),
   filenameHashing: false,
+  chainWebpack: config => {
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+  },
   configureWebpack: {
     entry: path.resolve(__dirname, "frontend", "src", "scripts", "main.js")
   },
