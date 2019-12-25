48eff6891ac0a38de5b49d47ad7a5cbffb465b3c
Eli Mordechai <emor0219@gmail.com>
2019-12-25 01:40:31 -0500

    revert cli service

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,6 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "app", "assets", "javascripts", "dist"),
   filenameHashing: false,
-  chainWebpack: config => {
-    config.plugins.delete('html')
-    config.plugins.delete('preload')
-    config.plugins.delete('prefetch')
-  },
   configureWebpack: {
     entry: path.resolve(__dirname, "frontend", "src", "scripts", "main.js")
   },
